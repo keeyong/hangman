7e24365a94dbc836c87823f7b24f89db13f38078
user <user@localhost.localdomain>
2015-01-22 09:31:13 +0900

    changed the message again

--- hangman.py
+++ hangman.py
@@ -123,7 +123,7 @@ def checkWrongAnswer(missedLetters, secretWord):
             
 def main():
     """Main application entry point."""
-    print('H A N G M A N - by keeyong')
+    print('H A N G M A N - by keeyong v2')
     missedLetters = ''
     correctLetters = ''
     gameSucceeded = False
