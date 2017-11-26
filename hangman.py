63e62b0063264bf675ac8b15fe8c468447c9006a
soyoung677 <lsy141@gmail.com>
2017-11-26 11:50:19 -0800

    modified by soyoung677

--- hangman.py
+++ hangman.py
@@ -123,7 +123,7 @@ def checkWrongAnswer(missedLetters, secretWord):
             
 def main():
     """Main application entry point."""
-    print('H A N G M A N by ...')
+    print('H A N G M A N by soyoung677')
     missedLetters = ''
     correctLetters = ''
     gameSucceeded = False
