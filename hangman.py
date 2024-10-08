55bb36f80e1571458b7b1f34ae71f7c00073a8fa
Keeyong (Max) <keeyonghan@hotmail.com>
2024-10-07 19:39:16 -0700

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -123,7 +123,7 @@ def checkWrongAnswer(missedLetters, secretWord):
             
 def main():
     """Main application entry point."""
-    print('H A N G M A N by ...')
+    print('H A N G M A N by Keeyong Han')
     missedLetters = ''
     correctLetters = ''
     gameSucceeded = False
